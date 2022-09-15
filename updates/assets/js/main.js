5a400b00b028ea57c71a9ab600ecbdeb34dd95f9
abdiyu <79357210+Abdiyu-urgesa@users.noreply.github.com>
2022-09-15 21:16:50 +0300

    Add files via upload

---
+++ updates/assets/js/main.js
@@ -0,0 +1,108 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 50,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+
+
+
+
+// image modal
+
+const open= document.getElementById('open');
+const modal_container = document.getElementById('modal-container');
+const close= document.getElementById ('close');
+
+open.addEventListener('click',()=>{
+  modal_container.classList.add('show');
+});
+
+close.addEventListener('click',()=>{
+  modal_container.classList.remove('show');
+});
+
+// image modal2
+
+const open2= document.getElementById('open2');
+const modal_container2 = document.getElementById('modal-container2');
+const close2= document.getElementById ('close2');
+
+open2.addEventListener('click',()=>{
+  modal_container2.classList.add('show');
+});
+
+close2.addEventListener('click',()=>{
+  modal_container2.classList.remove('show');
+});
+
+
+// image modal3
+
+const open3= document.getElementById('open3');
+const modal_container3 = document.getElementById('modal-container3');
+const close3= document.getElementById ('close3');
+
+open3.addEventListener('click',()=>{
+  modal_container3.classList.add('show');
+});
+
+close3.addEventListener('click',()=>{
+  modal_container3.classList.remove('show');
+});
